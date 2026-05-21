3a8aeca4783244dd8da46967adc0f4fbb0821959
Maksym Zabavchuk <zabavchukmaks21@gmail.com>
2026-05-21 16:29:20 +0200

    Remove: Jak kupić auto, Finansowanie links and YouTube icon from footer

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -379,7 +379,6 @@
             <div class="footer-top-socials">
                 <a href="#" class="footer-social" aria-label="Facebook"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="#1877f2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg></a>
                 <a href="#" class="footer-social" aria-label="Instagram"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="#e4405f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"/></svg></a>
-                <a href="#" class="footer-social" aria-label="YouTube"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/><path d="m10 15 5-3-5-3z"/></svg></a>
             </div>
         </div>
 
@@ -433,8 +432,6 @@
                 <ul>
                     <li><a href="#">Polityka prywatności</a></li>
                     <li><a href="#">Regulamin</a></li>
-                    <li><a href="#">Jak kupić auto?</a></li>
-                    <li><a href="#">Finansowanie</a></li>
                 </ul>
             </div>
 
